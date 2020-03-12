577e5dafac3e3dc48a78ebea5c64017bf0515501
hlyrastec <hlyrastec@gmail.com>
2020-03-12 19:22:50 -0300

    change product name lenght from 15 to 30

--- app/controller/product.js
+++ app/controller/product.js
@@ -49,7 +49,7 @@ const productController = {
 		};
 
 		if(!product.code || product.code < 1 || product.code > 9999){return res.send({ msg: 'Código de produto inválido.' })};
-		if(!product.name || product.name.length > 15){return res.send({ msg: 'Preencha o nome do produto.' })};
+		if(!product.name || product.name.length > 30){return res.send({ msg: 'Preencha o nome do produto.' })};
 		if(!product.color || product.color.length > 10){return res.send({ msg: 'Preencha a cor do produto.' })};
 		if(!product.size || product.size.length > 3){return res.send({ msg: 'Preencha o tamanho do produto.' })};
 
