6ea0cbb3944a16f91e850d5aec59ba7f6da22bd1
tijariomilitar <tijariomilitar@gmail.com>
2020-12-07 23:46:05 -0300

    sale implementing

--- app/controller/sale.js
+++ app/controller/sale.js
@@ -1,11 +1,23 @@
+const User = require('../model/user');
 const userController = require('./user');
+
+const lib = require('../../config/lib');
+
+const Product = require('../model/product');
 
 const saleController = {
 	index: async (req, res) => {
 		if(!await userController.verifyAccess(req, res, ['adm'])){
 			return res.redirect('/');
 		};
-		res.render('sale/index', { user: req.user });
+
+		try {
+			const productColors = await Product.colorList();
+			res.render('sale/index', { productColors, user: req.user });
+		} catch (err) {
+			console.log(err);
+			res.send({ msg: "Ocorreu um erro ao realizar requisição." });
+		};
 	}
 };
 
