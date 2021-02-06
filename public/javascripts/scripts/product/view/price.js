b5881da2080da88b9e934a44da4a02554360e4a9
tijariomilitar <tijariomilitar@gmail.com>
2021-02-06 08:58:40 -0300

    change close menu btn

--- public/javascripts/scripts/product/view/price.js
+++ public/javascripts/scripts/product/view/price.js
@@ -1,25 +1,5 @@
 Product.price.view = {};
 
-// Product.price.view.filter = (packages, pagination) => {
-// 	if(packages.length){
-// 		let html = "";
-// 		for (let i = pagination.page * pagination.pageSize; i < packages.length && i < (pagination.page + 1) * pagination.pageSize; i++){
-// 			html += "<div class='box one container border padding-5 margin-top-5'>";
-// 				html += "<div class='mobile-box nine center'><h3 class='tbl-show-link nowrap' onclick='Product.controller.package.show("+packages[i].id+")'>"+packages[i].code+"</h3></div>";
-// 				html += "<div class='mobile-box two center'>"+packages[i].name+"</div>";
-// 				html += "<div class='mobile-box nine center'>"+packages[i].color+"</div>";
-// 				html += "<div class='mobile-box nine center'>$"+packages[i].price+"</div>";
-// 				html += "<div class='mobile-box twelve center'><img class='img-tbl-btn' src='/images/icon/edit.png' onclick='Product.controller.package.edit("+packages[i].id+")'></div>";
-// 				html += "<div class='mobile-box twelve center'><img class='img-tbl-btn' src='/images/icon/trash.png' onclick='Product.controller.package.delete("+packages[i].id+")'></div>";
-// 			html += "</div>";
-// 		};
-// 		document.getElementById("product-package-filter-box").style.display = "";
-// 		document.getElementById("product-package-filter-div").innerHTML = html;
-// 	} else {
-// 		document.getElementById("product-package-filter-box").style.display = "";
-// 		document.getElementById("product-package-filter-div").innerHTML = "Sem resultados";
-// 	};
-// };
 Product.price.category.view = {};
 
 Product.price.category.view.filter = (categories, pagination) => {
@@ -39,4 +19,8 @@ Product.price.category.view.filter = (categories, pagination) => {
 		document.getElementById("product-price-category-filter-box").style.display = "";
 		document.getElementById("product-price-category-filter-div").innerHTML = "Sem resultados";
 	};
+};
+
+Product.price.category.view.show = (category) => {
+	
 };
