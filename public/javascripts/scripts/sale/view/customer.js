21791a2fd7bba60ba8100046282130bf7932ab4d
tijariomilitar <tijariomilitar@gmail.com>
2022-03-23 13:41:15 -0300

    show customer id when new sale

--- public/javascripts/scripts/sale/view/customer.js
+++ public/javascripts/scripts/sale/view/customer.js
@@ -5,11 +5,11 @@ Sale.view.customer.fillInput = (customers) => {
 	html += "<option value='' disabled selected>Selecionar cliente</option>";
 	for(i in customers){
 		if(customers[i].name){
-			html += "<option value='"+customers[i].id+"'>"+customers[i].name+" | "+customers[i].cnpj+"</option>";
+			html += "<option value='"+customers[i].id+"'>"+customers[i].id+" | "+customers[i].name+" | "+customers[i].cnpj+"</option>";
 		} else if(customers[i].brand && !customers[i].name){
-			html += "<option value='"+customers[i].id+"'>"+customers[i].brand+" | "+customers[i].cnpj+"</option>";
+			html += "<option value='"+customers[i].id+"'>"+customers[i].id+" | "+customers[i].brand+" | "+customers[i].cnpj+"</option>";
 		} else if(customers[i].trademark && !customers[i].name && !customers[i].brand){
-			html += "<option value='"+customers[i].id+"'>"+customers[i].trademark+" | "+customers[i].cnpj+"</option>";
+			html += "<option value='"+customers[i].id+"'>"+customers[i].id+" | "+customers[i].trademark+" | "+customers[i].cnpj+"</option>";
 		};
 	};
 	document.getElementById("sale-customer").innerHTML = html;
