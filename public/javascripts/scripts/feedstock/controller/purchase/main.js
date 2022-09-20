b86c848e0bc9d866bec55fddc506f85e87c392ef
tijariomilitar <tijariomilitar@gmail.com>
2022-09-19 22:20:02 -0300

    implemented metrics on feedstock purchase filter

--- public/javascripts/scripts/feedstock/controller/purchase/main.js
+++ public/javascripts/scripts/feedstock/controller/purchase/main.js
@@ -6,7 +6,7 @@ Feedstock.purchase.controller.setSupplier = (supplier_id) => {
 	document.getElementById("purchase-feedstock-add-form").elements.namedItem("feedstock").value = "";
 	document.getElementById("purchase-feedstock-add-form").elements.namedItem("amount").value = "";
 
-	if(!supplier_id){ return document.getElementById("purchase-feedstock-add-form").style.display = "none"; }
+	if (!supplier_id) { return document.getElementById("purchase-feedstock-add-form").style.display = "none"; }
 	document.getElementById("purchase-feedstock-add-form").style.display = "";
 };
 
@@ -24,7 +24,7 @@ Feedstock.purchase.controller.confirm = async () => {
 	};
 
 	purchase = await API.response(Feedstock.purchase.save, purchase);
-	if(!purchase) { return false; }
+	if (!purchase) { return false; }
 
 	document.getElementById("purchase-id").value = "";
 	document.getElementById("purchase-supplier-id").value = "";
@@ -35,56 +35,57 @@ Feedstock.purchase.controller.confirm = async () => {
 	Feedstock.purchase.controller.kart.update("code");
 	Feedstock.purchase.controller.kart.list("Feedstock.purchase.controller.kart", Feedstock.purchase.controller.kart.props);
 
-	if(document.getElementById("feedstock-purchase-kart")){ Feedstock.purchase.controller.filter.submit.click(); }
+	if (document.getElementById("feedstock-purchase-kart")) { Feedstock.purchase.controller.filter.submit.click(); }
 };
 
 Feedstock.purchase.controller.shipmentValue = document.getElementById("purchase-feedstock-kart-shipment-value");
-if(Feedstock.purchase.controller.shipmentValue){
+if (Feedstock.purchase.controller.shipmentValue) {
 	Feedstock.purchase.controller.shipmentValue.addEventListener("change", event => {
 		event.preventDefault();
 
 		Feedstock.purchase.controller.kart.update("code");
-	})
+	});
 };
 
 Feedstock.purchase.controller.discountValue = document.getElementById("purchase-feedstock-kart-discount-value");
-if(Feedstock.purchase.controller.discountValue){
+if (Feedstock.purchase.controller.discountValue) {
 	Feedstock.purchase.controller.discountValue.addEventListener("change", event => {
 		event.preventDefault();
 
 		Feedstock.purchase.controller.kart.update("code");
-	})
+	});
 };
 
 Feedstock.purchase.controller.filter = document.getElementById("purchase-feedstock-filter-form");
-if(Feedstock.purchase.controller.filter){
+if (Feedstock.purchase.controller.filter) {
 	Feedstock.purchase.controller.filter.addEventListener("submit", async event => {
 		event.preventDefault();
 
 		let purchase = {
 			code: event.target.elements.namedItem("code").value,
 			supplier_id: event.target.elements.namedItem("supplier-id").value,
-			period_start: lib.datetimeToTimestamp(event.target.elements.namedItem("period-start").value),
-			period_end: lib.datetimeToTimestamp(event.target.elements.namedItem("period-end").value)
+			period_start: lib.dateToTimestamp(event.target.elements.namedItem("period-start").value),
+			period_end: parseFloat(lib.dateToTimestamp(event.target.elements.namedItem("period-end").value)) + parseFloat(lib.timestampDay())
 		};
 
 		let purchases = await API.response(Feedstock.purchase.filter, purchase);
-		if(!purchases) { return false; }
+		if (!purchases) { return false; }
 
-		document.getElementById("purchase-feedstock-show-box").style.display = "none";
-		document.getElementById("feedstock-purchase-kart").style.display = "none";
+		lib.display("purchase-feedstock-show-box", "none");
+		lib.display("feedstock-purchase-kart", "none");
+		lib.display("purchase-feedstock-filter-box", "");
 
 		const pagination = { pageSize: 10, page: 0 };
-		(function(){ lib.carousel.execute("purchase-feedstock-filter-box", Feedstock.purchase.view.filter, purchases, pagination); }());
+		(function () { lib.carousel.execute("purchase-feedstock-filter-box", Feedstock.purchase.view.filter, purchases, pagination); }());
 	});
 }
 
 Feedstock.purchase.controller.open = async (purchase_id) => {
 	let purchases = await API.response(Feedstock.purchase.filter, { id: purchase_id });
-	if(!purchases) { return false; }
+	if (!purchases) { return false; }
 
 	purchases[0].feedstocks = await API.response(Feedstock.purchase.feedstock.filter, { purchase_id: purchase_id });
-	if(!purchases[0].feedstocks) { return false; }
+	if (!purchases[0].feedstocks) { return false; }
 
 	document.getElementById("purchase-feedstock-filter-box").style.display = "none";
 
@@ -93,21 +94,21 @@ Feedstock.purchase.controller.open = async (purchase_id) => {
 
 Feedstock.purchase.controller.edit = async purchase_id => {
 	let purchases = await API.response(Feedstock.purchase.filter, { id: purchase_id });
-	if(!purchases) { return false; }
+	if (!purchases) { return false; }
 
 	purchases[0].feedstocks = await API.response(Feedstock.purchase.feedstock.filter, { purchase_id: purchase_id });
-	if(!purchases[0].feedstocks) { return false; }
+	if (!purchases[0].feedstocks) { return false; }
 
 
-	if(purchases[0].status == "Ag. aprovação"){
+	if (purchases[0].status == "Ag. aprovação") {
 		let r = confirm("Deseja retornar o pedido para Em orçamento?");
-		if(r){
-			if(!await API.response(Feedstock.purchase.update, { id: purchase_id, status: 'Em orçamento' })){ return false; };
+		if (r) {
+			if (!await API.response(Feedstock.purchase.update, { id: purchase_id, status: 'Em orçamento' })) { return false; };
 			Feedstock.purchase.controller.filter.submit.click();
 		}
-	} else if(purchases[0].status == "Em orçamento") {
+	} else if (purchases[0].status == "Em orçamento") {
 		Feedstock.purchase.view.edit(purchases[0]);
-		
+
 		document.getElementById("purchase-feedstock-filter-box").style.display = "none";
 		document.getElementById("feedstock-purchase-kart").style.display = "";
 	}
@@ -115,8 +116,8 @@ Feedstock.purchase.controller.edit = async purchase_id => {
 
 Feedstock.purchase.controller.delete = async (purchase_id) => {
 	let r = confirm('Deseja realmente excluir a compra? Essa ação não pode ser revertida!');
-	if(r){
-		if(!await API.response(Feedstock.purchase.delete, purchase_id)){ return false; };
+	if (r) {
+		if (!await API.response(Feedstock.purchase.delete, purchase_id)) { return false; };
 		Feedstock.purchase.controller.filter.submit.click();
 	}
 };
--- public/javascripts/scripts/feedstock/view/purchase.js
+++ public/javascripts/scripts/feedstock/view/purchase.js
@@ -4,47 +4,53 @@ Feedstock.purchase.view.kart = () => {
 	let kart_box = document.getElementById("purchase-feedstock-kart-box");
 	Feedstock.purchase.controller.kart.items.length && lib.display(kart_box, "");
 	!Feedstock.purchase.controller.kart.items.length && lib.display(kart_box, "none");
-	
+
 	let kart_value = document.getElementById("purchase-feedstock-kart-value");
-	kart_value.innerHTML = "R$"+lib.roundValue(Feedstock.purchase.controller.kart.value).toFixed(2);
+	kart_value.innerHTML = "R$" + lib.roundValue(Feedstock.purchase.controller.kart.value).toFixed(2);
 
 	let shipment_value = document.getElementById("purchase-feedstock-kart-shipment-value");
-	shipment_value.innerHTML = "R$"+lib.roundValue(Feedstock.purchase.controller.kart.shipment_value).toFixed(2);
+	shipment_value.innerHTML = "R$" + lib.roundValue(Feedstock.purchase.controller.kart.shipment_value).toFixed(2);
 
 	let discount_value = document.getElementById("purchase-feedstock-kart-discount-value");
-	discount_value.innerHTML = "R$"+lib.roundValue(Feedstock.purchase.controller.kart.shipment_value).toFixed(2);
+	discount_value.innerHTML = "R$" + lib.roundValue(Feedstock.purchase.controller.kart.shipment_value).toFixed(2);
 
 	let kart_total_value = document.getElementById("purchase-feedstock-kart-total-value");
-	kart_total_value.innerHTML = "R$"+lib.roundValue(Feedstock.purchase.controller.kart.total_value).toFixed(2);
+	kart_total_value.innerHTML = "R$" + lib.roundValue(Feedstock.purchase.controller.kart.total_value).toFixed(2);
 };
 
 Feedstock.purchase.view.filter = (purchases, pagination) => {
 	let filter_div = document.getElementById("purchase-feedstock-filter-div");
 	filter_div.innerHTML = "";
 
-	if(purchases.length){
-		for (let i = pagination.page * pagination.pageSize; i < purchases.length && i < (pagination.page + 1) * pagination.pageSize; i++){
-			let purchase_div = lib.element.create("div", { class: "box b1 container padding-5 margin-top-5 border-explicit" });
+	const metrics = {
+		totalValue: 0
+	};
+
+	purchases.forEach(purchase => { metrics.totalValue += purchase.total_value; });
+
+	filter_div.append(lib.element.create("div", { class: "box b1 center lucida-grande bold padding-5 margin-top-5" }, `Gasto total: R$${metrics.totalValue.toFixed()}`));
+
+	if (purchases.length) {
+		for (let i = pagination.page * pagination.pageSize; i < purchases.length && i < (pagination.page + 1) * pagination.pageSize; i++) {
+			let purchase_div = lib.element.create("div", { class: "box b2 container padding-5 margin-top-5 border-explicit" });
 
 			purchase_div.appendChild(lib.element.create("div", {
 				class: "mobile-box b8 em11 tbl-show-link nowrap center bold",
-				onclick: "Feedstock.purchase.controller.open("+purchases[i].id+")"
+				onclick: "Feedstock.purchase.controller.open(" + purchases[i].id + ")"
 			}, purchases[i].id));
 
 			purchase_div.appendChild(lib.element.info("b2", "Fornecedor", purchases[i].supplier_brand));
 			purchase_div.appendChild(lib.element.info("b3-8", "Data da compra", lib.timestampToFulldate(purchases[i].date)));
 			purchase_div.appendChild(lib.element.info("b4", "Usuário", purchases[i].user_name));
 			purchase_div.appendChild(lib.element.info("b3", "Status", purchases[i].status));
-			purchase_div.appendChild(lib.element.info("b4 bold", "Valor da compra", "R$"+purchases[i].total_value.toFixed(2)));
+			purchase_div.appendChild(lib.element.info("b4 bold", "Valor da compra", "R$" + purchases[i].total_value.toFixed(2)));
 
-			purchase_div.appendChild(lib.element.icon('b12', 20, "/images/icon/edit.png", "Feedstock.purchase.controller.edit("+purchases[i].id+")"));
-			purchase_div.appendChild(lib.element.icon('b12', 20, "/images/icon/trash.png", "Feedstock.purchase.controller.delete("+purchases[i].id+")"));
+			purchase_div.appendChild(lib.element.icon('b12', 20, "/images/icon/edit.png", "Feedstock.purchase.controller.edit(" + purchases[i].id + ")"));
+			purchase_div.appendChild(lib.element.icon('b12', 20, "/images/icon/trash.png", "Feedstock.purchase.controller.delete(" + purchases[i].id + ")"));
 
 			filter_div.appendChild(purchase_div);
 		};
 	}
-
-	document.getElementById("purchase-feedstock-filter-box").style.display = "";
 };
 
 Feedstock.purchase.view.show = (purchase) => {
@@ -75,17 +81,17 @@ Feedstock.purchase.view.show = (purchase) => {
 
 	financial_div.appendChild(lib.element.create("div", { class: "box b1 underline center margin-top-5" }, "Valores da Compra"));
 	financial_div.appendChild(lib.element.create("div", { class: "mobile-box b2 padding-3 margin-top-5" }, "Matérias-primas:"));
-	financial_div.appendChild(lib.element.create("div", { class: "mobile-box b2 padding-3 margin-top-5 bold" }, "R$"+purchase.value.toFixed(2)));
+	financial_div.appendChild(lib.element.create("div", { class: "mobile-box b2 padding-3 margin-top-5 bold" }, "R$" + purchase.value.toFixed(2)));
 	purchase.shipment_value && financial_div.appendChild(lib.element.create("div", { class: "mobile-box b2 padding-3" }, "Frete:"));
-	purchase.shipment_value && financial_div.appendChild(lib.element.create("div", { class: "mobile-box b2 padding-3 bold" }, "R$"+purchase.shipment_value.toFixed(2)));
+	purchase.shipment_value && financial_div.appendChild(lib.element.create("div", { class: "mobile-box b2 padding-3 bold" }, "R$" + purchase.shipment_value.toFixed(2)));
 	purchase.discount_value && financial_div.appendChild(lib.element.create("div", { class: "mobile-box b2 padding-3" }, "Desconto:"));
-	purchase.discount_value && financial_div.appendChild(lib.element.create("div", { class: "mobile-box b2 padding-3 bold" }, "R$"+purchase.discount_value.toFixed(2)));
+	purchase.discount_value && financial_div.appendChild(lib.element.create("div", { class: "mobile-box b2 padding-3 bold" }, "R$" + purchase.discount_value.toFixed(2)));
 	financial_div.appendChild(lib.element.create("div", { class: "mobile-box b2 padding-3" }));
 	financial_div.appendChild(lib.element.create("div", { class: "mobile-box b2 underline" }));
 	financial_div.appendChild(lib.element.create("div", { class: "mobile-box b2 padding-3 bold" }, "Valor Total"));
-	financial_div.appendChild(lib.element.create("div", { class: "mobile-box b2 padding-3 bold" }, "R$"+purchase.total_value.toFixed(2)));
+	financial_div.appendChild(lib.element.create("div", { class: "mobile-box b2 padding-3 bold" }, "R$" + purchase.total_value.toFixed(2)));
 
-	show_div.appendChild(supplier_div);	
+	show_div.appendChild(supplier_div);
 	show_div.appendChild(purchase_div);
 	show_div.appendChild(financial_div);
 
@@ -99,15 +105,15 @@ Feedstock.purchase.view.show = (purchase) => {
 		feedstock_div.appendChild(lib.element.info("b2", "Nome", feedstock.name));
 		feedstock_div.appendChild(lib.element.info("b4", "Cor", feedstock.color_name));
 
-		feedstock.uom == "cm" && feedstock_div.appendChild(lib.element.info("b4 bold", "Quantidade", feedstock.amount+"m"));
-		feedstock.uom == "un" && feedstock_div.appendChild(lib.element.info("b4 bold", "Quantidade", feedstock.amount+"un"));
-		
-		feedstock.uom == "cm" && feedstock_div.appendChild(lib.element.info("b4", "Rolos", feedstock.amount / (feedstock.unit / 100)+"un"));
-		feedstock.uom == "un" && feedstock_div.appendChild(lib.element.info("b4", "Pacotes", feedstock.amount / feedstock.unit+"un"));
-		
-		feedstock_div.appendChild(lib.element.info("b4 bold", "Preço", "R$"+lib.roundValue(feedstock.price).toFixed(2)));
+		feedstock.uom == "cm" && feedstock_div.appendChild(lib.element.info("b4 bold", "Quantidade", feedstock.amount + "m"));
+		feedstock.uom == "un" && feedstock_div.appendChild(lib.element.info("b4 bold", "Quantidade", feedstock.amount + "un"));
 
-		feedstock_div.appendChild(lib.element.info("b4 bold", "Total", "R$"+lib.roundValue(feedstock.amount * feedstock.price).toFixed(2)));
+		feedstock.uom == "cm" && feedstock_div.appendChild(lib.element.info("b4", "Rolos", feedstock.amount / (feedstock.unit / 100) + "un"));
+		feedstock.uom == "un" && feedstock_div.appendChild(lib.element.info("b4", "Pacotes", feedstock.amount / feedstock.unit + "un"));
+
+		feedstock_div.appendChild(lib.element.info("b4 bold", "Preço", "R$" + lib.roundValue(feedstock.price).toFixed(2)));
+
+		feedstock_div.appendChild(lib.element.info("b4 bold", "Total", "R$" + lib.roundValue(feedstock.amount * feedstock.price).toFixed(2)));
 
 		feedstock_box.appendChild(feedstock_div);
 	});
@@ -118,7 +124,7 @@ Feedstock.purchase.view.show = (purchase) => {
 };
 
 Feedstock.purchase.view.edit = purchase => {
-	if(purchase.status == "Em orçamento" || purchase.status == "Ag. aprovação"){
+	if (purchase.status == "Em orçamento" || purchase.status == "Ag. aprovação") {
 		Feedstock.purchase.controller.kart.items = [];
 
 		document.getElementById("purchase-id").value = purchase.id;
