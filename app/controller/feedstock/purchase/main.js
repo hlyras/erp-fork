eef2caf4f245ade8a661c9272f11eb5d64e77b49
tijariomilitar <tijariomilitar@gmail.com>
2022-09-26 17:32:54 -0300

    update feedstock filter order by date

--- app/controller/feedstock/purchase/main.js
+++ app/controller/feedstock/purchase/main.js
@@ -10,12 +10,12 @@ Feedstock.purchase = require('../../../model/feedstock/purchase');
 const purchaseController = {};
 
 purchaseController.index = async (req, res) => {
-	if(!await userController.verifyAccess(req, res, ['adm','pro-man'])){
+	if (!await userController.verifyAccess(req, res, ['adm', 'pro-man'])) {
 		return res.redirect('/');
 	};
 
 	try {
-		let suppliers = await Feedstock.supplier.filter([],[],[],[],[]);
+		let suppliers = await Feedstock.supplier.filter([], [], [], [], []);
 		res.render('feedstock/purchase/index', { user: req.user, suppliers });
 	} catch (err) {
 		console.log(err);
@@ -24,12 +24,12 @@ purchaseController.index = async (req, res) => {
 };
 
 purchaseController.manage = async (req, res) => {
-	if(!await userController.verifyAccess(req, res, ['adm', 'pro-man'])){
+	if (!await userController.verifyAccess(req, res, ['adm', 'pro-man'])) {
 		return res.redirect('/');
 	};
 
 	try {
-		let suppliers = await Feedstock.supplier.filter([],[],[],[],[]);
+		let suppliers = await Feedstock.supplier.filter([], [], [], [], []);
 		res.render('feedstock/purchase/manage', { user: req.user, suppliers });
 	} catch (err) {
 		console.log(err);
@@ -38,7 +38,7 @@ purchaseController.manage = async (req, res) => {
 };
 
 purchaseController.save = async (req, res) => {
-	if(!await userController.verifyAccess(req, res, ['adm', 'pro-man'])){
+	if (!await userController.verifyAccess(req, res, ['adm', 'pro-man'])) {
 		return res.redirect('/');
 	};
 
@@ -55,18 +55,18 @@ purchaseController.save = async (req, res) => {
 	purchase.user_id = req.user.id;
 	purchase.feedstocks = JSON.parse(req.body.feedstocks);
 
-	if(!purchase.status){ return res.send({ msg: "Por favor preencha o status da compra." })}
-	if(!purchase.payment_method){ return res.send({ msg: "Por favor preencha o método de pagamento da compra." }); }
-	if(isNaN(parseFloat(purchase.value))){ return res.send({ msg: "O Valor das matérias-primas é inválido." }); }
-	if(isNaN(parseFloat(purchase.shipment_value))){ return res.send({ msg: "Valor do frete é inválido." }); }
-	if(isNaN(parseFloat(purchase.discount_value))){ return res.send({ msg: "Valor do desconto é inválido." }); }
-	if(isNaN(parseFloat(purchase.total_value))){ return res.send({ msg: "Valor da venda é inválido." }); }
+	if (!purchase.status) { return res.send({ msg: "Por favor preencha o status da compra." }) }
+	if (!purchase.payment_method) { return res.send({ msg: "Por favor preencha o método de pagamento da compra." }); }
+	if (isNaN(parseFloat(purchase.value))) { return res.send({ msg: "O Valor das matérias-primas é inválido." }); }
+	if (isNaN(parseFloat(purchase.shipment_value))) { return res.send({ msg: "Valor do frete é inválido." }); }
+	if (isNaN(parseFloat(purchase.discount_value))) { return res.send({ msg: "Valor do desconto é inválido." }); }
+	if (isNaN(parseFloat(purchase.total_value))) { return res.send({ msg: "Valor da venda é inválido." }); }
 
 	try {
-		if(!purchase.id){
+		if (!purchase.id) {
 			let saved_purchase = await purchase.save();
 
-			purchase.feedstocks.forEach(async feedstock => { 
+			purchase.feedstocks.forEach(async feedstock => {
 				let item = new Feedstock.purchase.feedstock();
 				item.purchase_id = saved_purchase.insertId;
 				item.feedstock_id = feedstock.feedstock_id;
@@ -77,26 +77,26 @@ purchaseController.save = async (req, res) => {
 
 			res.send({ done: 'Compra cadastrada sucesso!' });
 		} else {
-			purchase.feedstock_actions = { add: [], update: [], remove: [] } ;
+			purchase.feedstock_actions = { add: [], update: [], remove: [] };
 
 			let strict_params = { keys: [], values: [] };
 			lib.Query.fillParam("purchase_feedstock.purchase_id", purchase.id, strict_params);
-			let purchase_feedstocks = await Feedstock.purchase.feedstock.filter([], [], [], [], strict_params, []);	
+			let purchase_feedstocks = await Feedstock.purchase.feedstock.filter([], [], [], [], strict_params, []);
 
 			purchase.feedstocks = purchase_feedstocks.reduce((feedstocks, feedstock) => {
-				for(let i in feedstocks){ if(feedstocks[i].feedstock_id == feedstock.feedstock_id){ return feedstocks; } };
+				for (let i in feedstocks) { if (feedstocks[i].feedstock_id == feedstock.feedstock_id) { return feedstocks; } };
 				purchase.feedstock_actions.remove.push(feedstock);
 				return feedstocks;
 			}, purchase.feedstocks);
 
 			purchase_feedstocks = purchase.feedstocks.reduce((feedstocks, feedstock) => {
-				for(let i in feedstocks){ if(feedstocks[i].feedstock_id == feedstock.feedstock_id){ purchase.feedstock_actions.update.push(feedstock); return feedstocks; }; };
+				for (let i in feedstocks) { if (feedstocks[i].feedstock_id == feedstock.feedstock_id) { purchase.feedstock_actions.update.push(feedstock); return feedstocks; }; };
 				purchase.feedstock_actions.add.push(feedstock);
 				return feedstocks;
 			}, purchase_feedstocks);
 
 			//add feedstocks
-			for(let i in purchase.feedstock_actions.add){
+			for (let i in purchase.feedstock_actions.add) {
 				let item = new Feedstock.purchase.feedstock();
 				item.purchase_id = purchase.id;
 				item.feedstock_id = purchase.feedstock_actions.add[i].feedstock_id;
@@ -106,7 +106,7 @@ purchaseController.save = async (req, res) => {
 			};
 
 			//update feedstocks
-			for(let i in purchase.feedstock_actions.update){
+			for (let i in purchase.feedstock_actions.update) {
 				let item = new Feedstock.purchase.feedstock();
 				item.purchase_id = purchase.id;
 				item.feedstock_id = purchase.feedstock_actions.update[i].feedstock_id;
@@ -116,7 +116,7 @@ purchaseController.save = async (req, res) => {
 			};
 
 			//remove feedstocks
-			for(let i in purchase.feedstock_actions.remove){
+			for (let i in purchase.feedstock_actions.remove) {
 				await Feedstock.purchase.feedstock.remove(purchase.feedstock_actions.remove[i]);
 			};
 
@@ -128,9 +128,9 @@ purchaseController.save = async (req, res) => {
 		res.send({ msg: "Ocorreu um erro ao cadastrar a matéria-prima, favor contatar o suporte" });
 	};
 };
- 
+
 purchaseController.filter = async (req, res) => {
-	if(!await userController.verifyAccess(req, res, ['adm', 'pro-man', 'man'])){
+	if (!await userController.verifyAccess(req, res, ['adm', 'pro-man', 'man'])) {
 		return res.send({ unauthorized: "Você não tem permissão para realizar esta ação!" });
 	};
 	//139561143
@@ -145,8 +145,8 @@ purchaseController.filter = async (req, res) => {
 	];
 
 	let inners = [
-		["cms_wt_erp.feedstock_supplier supplier","supplier.id","purchase.supplier_id"],
-		["cms_wt_erp.user user","user.id","purchase.user_id"]
+		["cms_wt_erp.feedstock_supplier supplier", "supplier.id", "purchase.supplier_id"],
+		["cms_wt_erp.user user", "user.id", "purchase.user_id"]
 	];
 
 	let period = { key: "date", start: req.body.period_start, end: req.body.period_end };
@@ -155,7 +155,7 @@ purchaseController.filter = async (req, res) => {
 	lib.Query.fillParam("purchase.id", req.body.id, strict_params);
 	lib.Query.fillParam("purchase.supplier_id", req.body.supplier_id, strict_params);
 
-	let order_params = [ ["purchase.id","DESC"] ];
+	let order_params = [["purchase.date", "DESC"]];
 
 	try {
 		let purchases = await Feedstock.purchase.filter(props, inners, period, [], strict_params, order_params);
@@ -167,7 +167,7 @@ purchaseController.filter = async (req, res) => {
 };
 
 purchaseController.updateStatus = async (req, res) => {
-	if(!await userController.verifyAccess(req, res, ['adm','pro-man'])){
+	if (!await userController.verifyAccess(req, res, ['adm', 'pro-man'])) {
 		return res.send({ unauthorized: "Você não tem permissão para realizar esta ação!" });
 	};
 
@@ -181,7 +181,7 @@ purchaseController.updateStatus = async (req, res) => {
 };
 
 purchaseController.delete = async (req, res) => {
-	if(!await userController.verifyAccess(req, res, ['adm','pro-man'])){
+	if (!await userController.verifyAccess(req, res, ['adm', 'pro-man'])) {
 		return res.send({ unauthorized: "Você não tem permissão para realizar esta ação!" });
 	};
 
@@ -190,7 +190,7 @@ purchaseController.delete = async (req, res) => {
 		lib.Query.fillParam("purchase.id", req.params.id, strict_params);
 		let purchase = await Feedstock.purchase.filter([], [], [], [], strict_params, []);
 
-		if(purchase[0].status == "Em orçamento"){
+		if (purchase[0].status == "Em orçamento") {
 			await Feedstock.purchase.delete(req.params.id);
 			await Feedstock.purchase.feedstock.deleteByPurchaseId(req.params.id);
 			res.send({ done: 'Compra excluída com sucesso!' });
