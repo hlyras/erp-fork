0ac2d98ee44be3547745e57c11cd7bd081a5ab81
tijariomilitar <tijariomilitar@gmail.com>
2022-11-03 09:58:02 -0300

    grand pro-man access to manage

--- app/controller/ecommerce/main.js
+++ app/controller/ecommerce/main.js
@@ -37,7 +37,7 @@ ecommerceController.gathering = async (req, res) => {
 };
 
 ecommerceController.manage = async (req, res) => {
-  if (!await userController.verifyAccess(req, res, ['adm', 'pro-man', 'log-pac', 'pro-ass', 'pro-sto'])) {
+  if (!await userController.verifyAccess(req, res, ['adm', 'pro-man', 'log-pac', 'pro-ass', 'pro-sto', 'adm-man'])) {
     return res.redirect('/');
   };
 
