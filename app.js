5f1ab105fd2d585a3a0c1204599e224646a5e162
tijariomilitar <tijariomilitar@gmail.com>
2023-09-10 09:08:02 -0300

    allow origin

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ const app = express();
 
 require('dotenv').config();
 
+// Middleware to enable CORS
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://earnest-mandazi-85ed13.netlify.app');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 app.use(cors({ credentials: true }));
 
 const bodyParser = require('body-parser');
