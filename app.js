95a79d89351a7e0ea352863b827ed20c218aeca2
tijariomilitar <tijariomilitar@gmail.com>
2023-09-10 09:12:50 -0300

    cors fix

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ require('dotenv').config();
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'https://earnest-mandazi-85ed13.netlify.app');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
